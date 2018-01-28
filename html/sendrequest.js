9aaee9c7d65166cbbd5c92d5e94097193ed98b2a
Michel Promonet <michel.promonet@free.fr>
2018-01-28 16:21:14 +0100

    extract sendrequest.js

---
+++ html/sendrequest.js
@@ -0,0 +1,22 @@
+function sendRequest(request,method,headers,data,onSuccess,onFailure,scope) {
+
+	console.log("HTTP call "+ method);
+	var verb = "GET";
+	if (data) {
+		verb = "POST";
+		data = JSON.stringify(data);
+	}
+	request(verb , method,
+		{	
+			body: data,
+			headers: headers
+		}).done( function (response) { 
+			if ( (response.statusCode === 200) && onSuccess ) {
+				onSuccess.call(scope,JSON.parse(response.body));
+			}
+			else if (onFailure) {
+				onFailure.call(scope,response.statusCode);
+			}
+		}
+	);
+}
