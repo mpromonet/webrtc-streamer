75a85823ac57a05aa0ab58b10b7c8092cc4bf342
Michel Promonet <michel.promonet@free.fr>
2018-02-11 15:41:21 +0100

    try to fix heroku deployment and start with aws

---
+++ .circleci/setup-heroku.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
+
+cat > ~/.netrc << EOF
+machine api.heroku.com
+  login $HEROKU_LOGIN
+  password $HEROKU_API_KEY
+EOF
+
+cat >> ~/.ssh/config << EOF
+VerifyHostKeyDNS yes
+StrictHostKeyChecking no
+EOF
