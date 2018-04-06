a9d40637ef877a9892c4652c4c7d6641ed9bd99f
Hamza Zia <ziahamza2007@gmail.com>
2018-04-06 22:04:12 +0500

    remember button state tracking

--- html/index.js
+++ html/index.js
@@ -1,52 +1,60 @@
 import Keyboard from '@novnc/novnc/core/input/keyboard';
 import Mouse from '@novnc/novnc/core/input/mouse';
 
+function onEvent(webrtcServer, obj) {
+    const message = JSON.stringify(obj);
+    if (!webrtcServer.dc) {
+        console.log('ignoring message: ', message);
+        return;
+    }
+    console.log('sending message: ', message);
+    webrtcServer.dc.send(message);
+}
+
+const onMouseEvent = (webrtcServer, clickEvent) => onEvent(webrtcServer, {
+    presses: [],
+    clicks: [clickEvent],
+});
+
+const onKeyboardEvent = (webrtcServer, keyEvent) => onEvent(webrtcServer, {
+    clicks: [],
+    presses: [keyEvent],
+})
+
 window.setupElement = function setupElement(elem, webrtcServer) {
     console.log('setting up webrtc element!', elem, webrtcServer);
-    function onEvent(obj) {
-        const message = JSON.stringify(obj);
-        if (!webrtcServer.dc) {
-            console.log('ignoring message: ', message);
-            return;
-        }
-        console.log('sending message: ', message);
-        webrtcServer.dc.send(message);
-    }
+    // Mouse button state
+    let buttonMask = 0;
 
     const keyboard = new Keyboard(elem);
     keyboard.onkeyevent = (keysym, code, down) => {
-        onEvent({
-            clicks: [],
-            presses: [{
-                down,
-                code,
-                keysym,
-            }],
+        onKeyboardEvent(webrtcServer, {
+            down,
+            code,
+            keysym,
         });
     };
 
     const mouse = new Mouse(elem);
     mouse.onmousebutton = (x, y, down, bmask) => {
         console.log('got back mouse button: ', x, y, down, bmask);
-        onEvent({
-            presses: [],
-            clicks: [{
-                x: Math.floor(1000 * (x / elem.clientWidth)),
-                y: Math.floor(1000 * (y / elem.clientHeight)),
-                button: down ? bmask : 0,
-            }]
-        });
+        if (down) {
+            buttonMask |= bmask;
+        } else {
+            buttonMask &= ~bmask;
+        }
+
+        const relX = Math.floor(1000 * (x / elem.clientWidth));
+        const relY = Math.floor(1000 * (y / elem.clientHeight));
+
+        onMouseEvent(webrtcServer, { x: relX, y: relY, button: buttonMask });
     };
+
     mouse.onmousemove = (x, y) => {
         console.log('on mouse move: ', x, y);
-        onEvent({
-            presses: [],
-            clicks: [{
-                x: Math.floor(1000 * (x / elem.clientWidth)),
-                y: Math.floor(1000 * (y / elem.clientHeight)),
-                button: 0,
-            }]
-        });
+        const relX = Math.floor(1000 * (x / elem.clientWidth));
+        const relY = Math.floor(1000 * (y / elem.clientHeight));
+        onMouseEvent(webrtcServer, { x: relX, y: relY, button: buttonMask });
     };
 
     mouse.grab();
