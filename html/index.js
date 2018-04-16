5e9b8292ae2124c84adfb1c58247a806dc737da0
Hamza Zia <ziahamza2007@gmail.com>
2018-04-17 01:01:03 +0500

    use upstrem library

--- html/index.js
+++ html/index.js
@@ -1,119 +1,25 @@
-import Keyboard from '@novnc/novnc/core/input/keyboard';
-import Mouse from '@novnc/novnc/core/input/mouse';
+import setupWebRTCProxy from 'webrtc-proxy-client';
 
-let pendingEvents = [];
-let sendEventTimeout = null;
-function flushEvents(webrtcServer) {
-    clearTimeout(sendEventTimeout);
-    sendEventTimeout = setTimeout(flushEvents.bind(null, webrtcServer), 200);
-    const events = filteredEvents(pendingEvents);
-    pendingEvents = [];
-    if (!events.length) {
-        console.log('no pending events in this time slot');
-        return;
-    }
-    const message = JSON.stringify({ events });
-    if (!webrtcServer.dc) {
-        console.log('ignoring message: ', message);
-        return;
-    }
-    
-    console.log('sending message: ', message);
-    webrtcServer.dc.send(message);
-}
-
-/*
-    Filter out all the mouse move events in between mouse button states.
-    e.g if first starts from (10, 20) then hovers to (10, 30), then clicks and drags
-    until (10, 40) then the filteredEvents will only keep events:
-    1. (10, 20) with button state static
-    2. (10, 30) with button state static
-    3. (10, 30) with button state dragged
-    4. (10, 40) with button state dragged
-*/
-function filteredEvents(pendingEvents) {
-    let skippedEvs = 0;
-    const { currentClick, events: evs } = pendingEvents.reduce(({ currentClick, events }, ev) => {
-        let newEvents = events;
-        if (ev.isClick) {
-            if (!currentClick) {
-                newEvents = [...newEvents, ev];
-            }
-            else if (currentClick.button !== ev.button) {
-                if (skippedEvs) {
-                    newEvents = [...newEvents, currentClick];
-                }
-            }
-            else {
-                skippedEvs++;
-            }
-
-            return {
-                currentClick: ev,
-                events: newEvents,
-            }
-        }
-
-        return { currentClick, events: [...events, ev] };
-    }, { currentClick: null, events: [] });
-    return currentClick ? [...evs, currentClick] : evs;
-}
-
-function onEvent(webrtcServer, event) {
-    pendingEvents.push(event);
-    // respond instantly when a keyboard press, mouse button press or initial event
-    if (event.isPress || !sendEventTimeout || event.button) {
-        flushEvents(webrtcServer);
-    }
-}
-
-const onMouseEvent = (webrtcServer, clickEvent) => onEvent(webrtcServer, {
-    ...clickEvent,
-    isClick: true,
-});
-
-const onKeyboardEvent = (webrtcServer, keyEvent) => onEvent(webrtcServer, {
-    ...keyEvent,
-    isPress: true,
-});
-
-window.setupElement = function setupElement(elem, webrtcServer) {
-    console.log('setting up webrtc element!', elem, webrtcServer);
-    // Mouse button state
-    let buttonMask = 0;
-
-    const keyboard = new Keyboard(document);
-    keyboard.onkeyevent = (keysym, code, down) => {
-        console.log('got back keyboard event: ', keysym, code, down);
-        onKeyboardEvent(webrtcServer, {
-            down,
-            code: keysym,
-        });
-    };
-
-    const mouse = new Mouse(elem);
-    mouse.onmousebutton = (x, y, down, bmask) => {
-        console.log('got back mouse button: ', x, y, down, bmask);
-        if (down) {
-            buttonMask |= bmask;
+(function init() {
+    if (!location.search.slice(1)) {
+        if (typeof URLSearchParams != 'undefined') {
+            alert("WebRTC stream name to connect is missing\n\nUsage :" + window.location + "?video=<WebRTC video stream name>&audio=<WebRTC audio stream name>&options=<WebRTC options>")
         } else {
-            buttonMask &= ~bmask;
+            alert("WebRTC stream name to connect is missing\n\nUsage :" + window.location + "?<WebRTC video stream name>")
         }
+        return;
+    }
 
-        const relX = Math.floor(1000 * (x / elem.clientWidth));
-        const relY = Math.floor(1000 * (y / elem.clientHeight));
+    let webrtcServer      = new WebRtcStreamer("video", webrtcConfig.url, request);
+    let url = { video:location.search.slice(1) };
+    let options;
+    if (typeof URLSearchParams != 'undefined') {
+        let params = new URLSearchParams(location.search.slice(1));
+        if (params.has("video") || params.has("audio")) {
+            url = { video:params.get("video"), audio:params.get("audio") };
+        }
+        options = params.get("options");
+    }
+    setupWebRTCProxy({ elemId: 'video', videoUrl: url.video, audioUrl: url.audio, proxyUrl: '', options });
+})();
 
-        onMouseEvent(webrtcServer, { x: relX, y: relY, button: buttonMask });
-    };
-
-    mouse.onmousemove = (x, y) => {
-        console.log('on mouse move: ', x, y);
-        const relX = Math.floor(1000 * (x / elem.clientWidth));
-        const relY = Math.floor(1000 * (y / elem.clientHeight));
-        onMouseEvent(webrtcServer, { x: relX, y: relY, button: buttonMask });
-    };
-
-    mouse.grab();
-    keyboard.grab();
-    console.log('grabbed keyboard successfully!: ', keyboard);
-}
--- html/webpack.config.js
+++ html/webpack.config.js
@@ -2,6 +2,20 @@ const path = require('path');
 
 module.exports = {
   entry: './index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules(?!\/webpack-proxy-client)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   mode: 'development',
   output: {
     filename: 'index.js',
