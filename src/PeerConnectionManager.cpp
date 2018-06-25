bcb73a1ad88ef3eb75ce1a6bf1b8f0d3193138ff
Hamza Zia <ziahamza2007@gmail.com>
2018-06-25 14:55:37 +0500

    removed redundant code

--- src/PeerConnectionManager.cpp
+++ src/PeerConnectionManager.cpp
@@ -322,20 +322,6 @@ const Json::Value PeerConnectionManager::createOffer(const std::string &peerid, 
 	{
 		rtc::scoped_refptr<webrtc::PeerConnectionInterface> peerConnection = peerConnectionObserver->getPeerConnection();
 
-		// set bandwidth
-		std::string tmp;
-		if (CivetServer::getParam(options, "bitrate", tmp)) {
-			int bitrate = std::stoi(tmp);
-
-			webrtc::PeerConnectionInterface::BitrateParameters bitrateParam;
-			bitrateParam.min_bitrate_bps = rtc::Optional<int>(bitrate/2);
-			bitrateParam.current_bitrate_bps = rtc::Optional<int>(bitrate);
-			bitrateParam.max_bitrate_bps = rtc::Optional<int>(bitrate*2);
-			peerConnection->SetBitrate(bitrateParam);
-
-			RTC_LOG(WARNING) << "set bitrate:" << bitrate;
-		}
-
 		if (!this->AddStreams(peerConnectionObserver->getPeerConnection(), videourl, audiourl, peerid, options))
 		{
 			RTC_LOG(WARNING) << "Can't add stream";
