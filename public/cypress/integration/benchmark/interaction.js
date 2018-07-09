9f4fcf4716226e1318832d7b504db5917e93af30
Hamza Zia <ziahamza2007@gmail.com>
2018-07-10 02:20:21 +0800

    updated cypress interactions

--- public/cypress/integration/benchmark/interaction.js
+++ public/cypress/integration/benchmark/interaction.js
@@ -1,6 +1,14 @@
+function move(x, y) {
+    cy.get('video')
+      .trigger('mouseover', x, y, {force: true})
+}
+
 describe('benchmarking proxy', () => {
   it('interacts properly', () => {
     cy.visit(`${Cypress.env('PROXY_URL')}/index.html?video=${Cypress.env('VNC_URL')}`)
     cy.get('video', { timeout: 100000 }).invoke('width').should('be.gt', 1000)
+
+    cy.get('video').click(500, 50, {force: true}) 
+    cy.get('video').type('www.murcul.com{enter}', { force: true })
   });
 });
