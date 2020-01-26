05e27586d2b93f608f038245b9878dae7d980c75
mpromonet <michel.promonet@free.fr>
2020-01-26 09:10:02 +0100

    fix style coding issue

--- prepublish.js
+++ prepublish.js
@@ -1,20 +1,24 @@
 // compute version
-var getRepoInfo = require('git-repo-info');
+var getRepoInfo = require("git-repo-info");
 var info = getRepoInfo();
-var version="1.0.0-"+info.branch+"."+info.abbreviatedSha
+var version="1.0.0-"+info.branch+"."+info.abbreviatedSha;
 if (info.tag) {
   version=info.tag;
 } else if (info.lastTag) {
   version=info.lastTag;
-  if (info.commitsSinceLastTag) version+="-"+info.commitsSinceLastTag;
+  if (info.commitsSinceLastTag) {
+	  version+="-"+info.commitsSinceLastTag;
+  }
 }
 
 // update version
-var npm = require('npm');
+var npm = require("npm");
 npm.load({}, function (err) {
-        npm.config.set('git-tag-version',false)
-        npm.config.set('allow-same-version',true)
+        npm.config.set("git-tag-version",false);
+        npm.config.set("allow-same-version",true);
         npm.commands.version([version], (err) => {
-                if (err) console.log(err)
+                if (err) {
+			console.log(err);
+		}
         });
 });
