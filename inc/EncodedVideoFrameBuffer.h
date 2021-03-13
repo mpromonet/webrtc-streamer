2278e5bee1c78341153e1ae977b8cbccb0c98cb6
mpromonet <michel.promonet@free.fr>
2021-03-13 20:08:58 +0100

    little clean up

--- inc/EncodedVideoFrameBuffer.h
+++ inc/EncodedVideoFrameBuffer.h
@@ -13,41 +13,38 @@
 #include "rtc_base/ref_counted_object.h"
 #include "absl/strings/match.h"
 
-class EncodedVideoI420Buffer : public webrtc::I420BufferInterface {
- public:
-   EncodedVideoI420Buffer(int width, int height, const rtc::scoped_refptr<webrtc::EncodedImageBufferInterface>& encoded_data) : width_(width), height_(height), encoded_data_(encoded_data) {
+class EncodedVideoI420Buffer : public webrtc::I420BufferInterface
+{
+public:
+  EncodedVideoI420Buffer(int width, int height, const rtc::scoped_refptr<webrtc::EncodedImageBufferInterface> &encoded_data) : width_(width), height_(height), encoded_data_(encoded_data)
+  {
   }
   virtual int width() const { return width_; }
   virtual int height() const { return height_; }
-  virtual const uint8_t* DataY() const { return encoded_data_->data(); }
-  virtual const uint8_t* DataU() const { return encoded_data_->data(); }
-  virtual const uint8_t* DataV() const { return encoded_data_->data(); }
+  virtual const uint8_t *DataY() const { return encoded_data_->data(); }
+  virtual const uint8_t *DataU() const { return encoded_data_->data(); }
+  virtual const uint8_t *DataV() const { return encoded_data_->data(); }
   virtual int StrideY() const { return encoded_data_->size(); }
-  virtual int StrideU() const { return (encoded_data_->size()+1)/2; }
-  virtual int StrideV() const { return (encoded_data_->size()+1)/2; }
+  virtual int StrideU() const { return encoded_data_->size(); }
+  virtual int StrideV() const { return encoded_data_->size(); }
 
- private:
+private:
   const int width_;
-  const int height_;  
+  const int height_;
   rtc::scoped_refptr<webrtc::EncodedImageBufferInterface> encoded_data_;
 };
 
-class EncodedVideoFrameBuffer : public webrtc::VideoFrameBuffer {
- public:
-  EncodedVideoFrameBuffer(int width, int height, const rtc::scoped_refptr<webrtc::EncodedImageBufferInterface>& encoded_data) : width_(width), height_(height) {
-	  buffer_ = new rtc::RefCountedObject<EncodedVideoI420Buffer>(width_, height_, encoded_data);
-  }
+class EncodedVideoFrameBuffer : public webrtc::VideoFrameBuffer
+{
+public:
+  EncodedVideoFrameBuffer(int width, int height, const rtc::scoped_refptr<webrtc::EncodedImageBufferInterface> &encoded_data)
+    : buffer_(new rtc::RefCountedObject<EncodedVideoI420Buffer>(width, height, encoded_data)) {}
   virtual Type type() const { return webrtc::VideoFrameBuffer::Type::kNative; }
   virtual rtc::scoped_refptr<webrtc::I420BufferInterface> ToI420() { return webrtc::I420Buffer::Create(width(), height()); }
-  virtual int width() const { return width_; }
-  virtual int height() const { return height_; }
-  const webrtc::I420BufferInterface* GetI420() const final { return buffer_.get();  }
+  virtual int width() const { return buffer_->width(); }
+  virtual int height() const { return buffer_->height(); }
+  const webrtc::I420BufferInterface *GetI420() const final { return buffer_.get(); }
 
- private:
-  const int width_;
-  const int height_;  
+private:
   rtc::scoped_refptr<EncodedVideoI420Buffer> buffer_;
 };
-
-
-
