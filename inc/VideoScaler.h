2df0b08e8c7e86093ca36f29a48090ea39bf41e5
mpromonet <michel.promonet@free.fr>
2023-03-06 18:23:35 +0100

    try to fix windows pipeline

--- inc/VideoScaler.h
+++ inc/VideoScaler.h
@@ -83,6 +83,25 @@ public:
     {
     }
 
+    rtc::scoped_refptr<webrtc::I420Buffer> createOutputBuffer() {
+        int height = m_height;
+        int width = m_width;
+        if ( (height == 0) && (width == 0) )
+        {
+            height = m_roi_height;
+            width = m_roi_width;
+        }
+        else if (height == 0)
+        {
+            height = (m_roi_height * width) / m_roi_width;
+        }
+        else if (width == 0)
+        {
+            width = (m_roi_width * height) / m_roi_height;
+        }
+        return webrtc::I420Buffer::Create(width, height);
+    }
+
     void OnFrame(const webrtc::VideoFrame &frame) override
     {
         if ((m_roi_x != 0) && (m_roi_x >= frame.width()))
@@ -115,43 +134,21 @@ public:
             m_roi_height = frame.height() - m_roi_y;
         }
 
-        // source image is croped but destination image size is not set
-        if ((m_roi_width != frame.width() || m_roi_height != frame.height()) && (m_height == 0 && m_width == 0))
-        {
-            m_height = m_roi_height;
-            m_width = m_roi_width;
-        }
-
-        if ( ((m_height == 0) && (m_width == 0) && (m_rotation == webrtc::kVideoRotation_0)) || (frame.video_frame_buffer()->type() == webrtc::VideoFrameBuffer::Type::kNative) )
+        if ( ((m_roi_width != frame.width()) && (m_roi_height == frame.height()) && (m_rotation == webrtc::kVideoRotation_0)) || (frame.video_frame_buffer()->type() == webrtc::VideoFrameBuffer::Type::kNative) )
         {
             m_broadcaster.OnFrame(frame);
         }
         else
         {
-            int height = m_height;
-            int width = m_width;
-            if ( (height == 0) && (width == 0) )
-            {
-                height = frame.height();
-                width = frame.width();
-            }
-            else if (height == 0)
-            {
-                height = (m_roi_height * width) / m_roi_width;
-            }
-            else if (width == 0)
-            {
-                width = (m_roi_width * height) / m_roi_height;
-            }
-            rtc::scoped_refptr<webrtc::I420Buffer> scaled_buffer = webrtc::I420Buffer::Create(width, height);
+            rtc::scoped_refptr<webrtc::I420Buffer> scaled_buffer = this->createOutputBuffer();
             if (m_roi_width != frame.width() || m_roi_height != frame.height())
             {
-                RTC_LOG(LS_VERBOSE) << "crop:" << m_roi_x << "x" << m_roi_y << " " << m_roi_width << "x" << m_roi_height << " scale: " << width << "x" << height;
+                RTC_LOG(LS_VERBOSE) << "crop:" << m_roi_x << "x" << m_roi_y << " " << m_roi_width << "x" << m_roi_height << " scale: " << scaled_buffer->width() << "x" << scaled_buffer->height();
                 scaled_buffer->CropAndScaleFrom(*frame.video_frame_buffer()->ToI420(), m_roi_x, m_roi_y, m_roi_width, m_roi_height);
             }
             else
             {
-                RTC_LOG(LS_VERBOSE) << "scale: " << width << "x" << height;
+                RTC_LOG(LS_VERBOSE) << "scale: " << scaled_buffer->width() << "x" << scaled_buffer->height();
                 scaled_buffer->ScaleFrom(*frame.video_frame_buffer()->ToI420());
             }
             
